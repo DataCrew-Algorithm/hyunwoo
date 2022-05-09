61ba96da4720d3d36be8a6254edd0c3c91dca95f
hyunwoogo <cksl1220@naver.com>
2022-05-09 21:08:44 +0900

    update Leet1

--- Leet/week2/두 수의 합 - Leet1.py
+++ Leet/week2/두 수의 합 - Leet1.py
@@ -28,15 +28,13 @@
 
 #         return ans_lst
 
-
-
-
 # num = [3, 3]
 # target = 6
 
 # sol = Solution()
 # ans = sol.twoSum(num, target)
 # print(ans)
+
 
 # =====================================================================
 
@@ -59,3 +57,35 @@ target = 6
 sol = Solution()
 ans = sol.twoSum(num, target)
 print(ans)
+
+
+# =====================================================================
+
+# 수현님 아이디어로 고친 투포인터
+# 61ms
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        new_nums =sorted(nums)
+        left = 0
+        right = len(nums) - 1
+
+        while left < right:
+            if new_nums[left] + new_nums[right] < target:
+                left += 1
+            elif new_nums[left] + new_nums[right] > target:
+                right -= 1
+            else:
+                break
+        
+        if new_nums[left] == new_nums[right]:
+            left_ = nums.index(new_nums[left])
+            nums[left_] = "*"
+            right_ = nums.index(new_nums[right])
+
+        else:
+            left_ = nums.index(new_nums[left])
+            right_ = nums.index(new_nums[right])
+
+
+        return [left_, right_]
